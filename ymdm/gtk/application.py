af5298d1137e44e9f4176c4a80410cbae68b0e92
saka-p <linuxlover67229@gmail.com>
2026-06-04 15:43:50 -0400

    Remove invalid set_desktopfilename call

--- ymdm/gtk/application.py
+++ ymdm/gtk/application.py
@@ -11,8 +11,6 @@ class YmdmApplication(Adw.Application):
     def __init__(self):
         super().__init__(application_id="io.github.saka_p.ymdm")
         self.connect("activate", self._on_activate)
-        # Set the desktop file name so Wayland matches the correct icon
-        self.set_desktopfilename("ymdm")
 
     def _on_activate(self, app):
         win = YmdmWindow(app)
